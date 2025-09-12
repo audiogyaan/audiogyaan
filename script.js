d4fbf322d6f8c7f73f6f88834e4c02b0a4967da0
audiogyaan <samirgamerk94@gmail.com>
2025-09-12 17:01:43 +0530

    your message

--- script.js
+++ script.js
@@ -110,12 +110,13 @@ const booksData = {
   "The 48 Laws of Power": ["assets/grow1.mp3", "assets/grow2.mp3", "assets/grow3.mp3"],
   "Zero To One": ["assets/zero2one1.m4a", "assets/zero2one2.m4a", "assets/zero2one3.m4a"],
 
-  // ✅ missing comma fixed here
   "Eat That Froge": ["assets/eatthatfrog1.m4a", "assets/eatthatfrog2.m4a", "assets/eatthatfrog3.m4a"],
   "Dopamin Detox": ["assets/dopamin1.m4a", "assets/dopamin2.m4a", "assets/dopamin3.m4a"],
-  "Bhagwat Geeta": ["assets/gita1.m4a", "assets/gita2.m4a", "assets/gita3.m4a" , "assets/gita4.m4a", , "assets/gita5.m4a",],
-  "The Quran": ["assets/quran1.m4a", "assets/quran2.m4a", "assets/quran3.m4a" , "assets/quran4.m4a" , "assets/quran5.m4a"],
-   "Can We Be Strangers Again?": ["assets/strangerspart1.m4a", "assets/strangers2.m4a", "assets/strangers3.m4a", "assets/strangers4.m4a", "assets/strangers5.m4a",]
+  "Bhagwat Geeta": ["assets/gita1.m4a", "assets/gita2.m4a", "assets/gita3.m4a", "assets/gita4.m4a", "assets/gita5.m4a"],
+  "The Quran": ["assets/quran1.m4a", "assets/quran2.m4a", "assets/quran3.m4a", "assets/quran4.m4a", "assets/quran5.m4a"],
+  "Can We Be Strangers Again?": ["assets/strangerspart1.m4a", "assets/strangers2.m4a", "assets/strangers3.m4a", "assets/strangers4.m4a", "assets/strangers5.m4a"],
+  "I Dont Love You Any More": ["assets/love1.m4a", "assets/love2.m4a", "assets/love3.m4a"],
+  "Thank You For Leaving": ["assets/leave1.m4a", "assets/leave2.m4a", "assets/leave3.m4a"]
 };
 
 
